d9e42383d4826893af3b528f8f2578c99db718e3
zedohany <zedohany@outlook.con>
2025-09-04 19:01:13 +0300

    feat: add task routes with auth middleware and controller methods

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use function Pest\Laravel\get;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -13,7 +15,14 @@ Route::get('dashboard', function () {
 
 Route::get('vital', function () {
     return Inertia::render('Vital');
-});
+})->middleware(['auth', 'verified'])->name('vital');
+
+Route::get('tasks', function () {
+    return Inertia::render('Task');
+})->middleware(['auth', 'verified'])->name('tasks');
+
+Route::get('tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+Route::post('tasks/create', [TaskController::class, 'store'])->name('tasks.store');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
